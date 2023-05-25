514ec3cff03d35dc5d94e5b28e07e70476ce6f12
GarethRader <57623675+GarethRader@users.noreply.github.com>
2023-05-25 15:07:55 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import React from "react";
-import Login from "../components/auth/Login";
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +7,7 @@ const Home: NextPage = () => {
       <div className="h-1/3"></div>
       <div className="justify-center flex h1/3">
         <div className="border-8 p-16 rounded-full softshadow ">
-          <Login />
+          Hello Index
         </div>
       </div>
       <div className="h-1/3"></div>
